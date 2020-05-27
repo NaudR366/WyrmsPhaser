1181ea5c9ace2930ddb6af4ec9445c8e7bc1d29c
MarcvdMade <55003488+MarcvdMade@users.noreply.github.com>
2020-05-27 10:15:14 +0200

    made the game fullscreen and removed margins

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import HelloWorldScene from './scenes/HelloWorldScene'
 
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
+	width: window.innerWidth,
+	height: window.innerHeight,
 	physics: {
 		default: 'arcade',
 		arcade: {
 			gravity: { y: 200 }
-			debug: true,
+			// debug: true,
 		}
 	},
 	scene: [HelloWorldScene]
