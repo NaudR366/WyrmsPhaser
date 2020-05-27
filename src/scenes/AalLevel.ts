eaf3fa1181d32bfcf21167b7a939c9ed5e034ad0
NaudR366 <57040315+NaudR366@users.noreply.github.com>
2020-05-27 11:41:46 +0200

    AalLevel created

---
+++ src/scenes/AalLevel.ts
@@ -0,0 +1,27 @@
+import Phaser from 'phaser'
+
+export default class AalLevel extends Phaser.Scene {
+
+    
+
+    constructor()
+	{
+		super('aal-world')
+	}
+
+
+    preload() {
+
+    
+    }
+
+    create() {
+
+        
+    }
+
+    update() {
+        
+    }
+
+}
