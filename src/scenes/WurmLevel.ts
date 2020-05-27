968be1607cd0546f8c946f939dc3700dd701376c
MarcvdMade <55003488+MarcvdMade@users.noreply.github.com>
2020-05-27 11:51:12 +0200

    Added worm player to worm level

--- src/scenes/WurmLevel.ts
+++ src/scenes/WurmLevel.ts
@@ -3,6 +3,8 @@ import Phaser from 'phaser'
 export default class WurmLevel extends Phaser.Scene {
 
     private platforms?: Phaser.Physics.Arcade.StaticGroup
+    private player? : Phaser.Physics.Arcade.Sprite
+    private cursors?: Phaser.Types.Input.Keyboard.CursorKeys
 
     constructor()
 	{
@@ -14,6 +16,9 @@ export default class WurmLevel extends Phaser.Scene {
 
         this.load.image('background', 'assets/background.png')
         this.load.image('ground', 'assets/ground.png')
+        this.load.spritesheet('dude', 'assets/dude2.png', { 
+            frameWidth: 65, frameHeight: 60
+        })
     }
 
     create() {
@@ -22,18 +27,77 @@ export default class WurmLevel extends Phaser.Scene {
 
         //create platforms
         this.platforms = this.physics.add.staticGroup()
-        const ground = this.platforms.create(100, 200, 'ground') as Phaser.Physics.Arcade.Sprite
+        const ground = this.platforms.create(760, 750, 'ground') as Phaser.Physics.Arcade.Sprite
         
         ground
-        .setScale(3)
+        .setScale(4)
         .refreshBody()
 
         this.platforms.create(800,100, 'ground')
         this.platforms.create(50,250, 'ground')
-        this.platforms.create(750,220, 'ground')
+        this.platforms.create(1200,550, 'ground')
+
+        //create player
+        this.player = this.physics.add.sprite(100, 450, 'dude')
+        this.player.setBounce(0.1)
+        this.player.setCollideWorldBounds(true)
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', {
+                start: 0, end: 3
+            }),
+            frameRate: 10,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20
+        })
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.physics.add.collider(this.player, this.platforms)
+
+        this.cursors = this.input.keyboard.createCursorKeys()
+
     }
 
     update() {
+        //check keyboard inputs
+        if(!this.cursors)
+        {
+             return
+        }
+ 
+        if(this.cursors?.left?.isDown)
+        {
+            this.player?.setVelocityX(-260)
+            this.player?.anims.play('left',true)
+        } 
+        else if(this.cursors?.right?.isDown)
+        {
+            this.player?.setVelocityX(260)
+            this.player?.anims.play('right',true)
+        } else 
+        {
+         this.player?.setVelocityX(0)
+         this.player?.anims.play('turn')
+        }
+ 
+        if (this.cursors.up?.isDown && this.player?.body.touching.down) 
+        {
+             this.player.setVelocityY(-360)
+        }
+ 
+     } 
     }
 
 }
